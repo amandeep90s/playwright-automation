951c29d3e76676f0e709218aecb3826d9f564646
Amandeep Singh <65186944+amandeep90s@users.noreply.github.com>
2026-08-20 00:11:11 +0530

    fix: standardize quotes and improve test assertions in configuration and test files

--- tests/ui-basics-test.spec.js
+++ tests/ui-basics-test.spec.js
@@ -1,12 +1,18 @@
 // @ts-check
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("browser context playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://www.w3schools.com/");
+
+  await expect(page).toHaveTitle(/W3Schools/);
 });
 
 test("page playwright test", async ({ page }) => {
   await page.goto("https://www.w3schools.com/");
+
+  await expect(
+    page.getByRole("heading", { name: "Learn to Code" }),
+  ).toBeVisible();
 });
